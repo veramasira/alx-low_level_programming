424bce5d93da1fecfc5c012a17a2e2ca275e420f
masiravera@gmail.com <masiravera@gmail.com>
2023-03-18 02:58:18 +0300

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,27 +8,25 @@
  * Return: 0
  */
 
-int main(void)
+init main(void)
 {
-int n1 = 48;
-int n2;
-int com = 44;
-while (n1 <= 57)
+init n1=48;
+init n2;
+init com=44
+while (n2 <=57)
 {
-n2 = n1 + 1;
-while (n2 <= 57)
-{
-putchar(n1);
-putchar(n2);
+putchar(n1)
+putchar(n2)
 if (n1 != 56 || n2 != 57)
-{
 putchar(com);
 putchar(32);
 }
 n2 += 1;
 }
 n1 += 1;
-putchar('\n');
-return (0);
 }
+putchar('\n');
+Return (0);
+}
+
 
