086a38c9bc3978fe6ceae143c4903efa2b42b299
masiravera@gmail.com <masiravera@gmail.com>
2023-03-17 19:04:30 +0300

    100-intel

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,12 @@
-#!/bin/bash
 #include <stdio.h>
-
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ * main - A program printing a line with puts function
+ * Return: 0
+ */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
-}
+	("\"puts Programming is like building a multilingual puzzle");
+return(0)
+}	
 
 
