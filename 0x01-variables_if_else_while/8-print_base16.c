2db6840aea0ffb88ca8da76d128d77f997f6ca09
masiravera@gmail.com <masiravera@gmail.com>
2023-03-19 15:41:02 +0300

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,26 +1,22 @@
-include <stdio.h>
+#include <stdio.h>
 
 /**
- * main -  program that prints all the numbers of base 16 in lowercase
- * You can only use the putchar function
- * Return: 0
+ * main - Prints all single digit numbers of base 16in lowercase.
+ *
+ * Return: Always 0.
  */
+init main(void)
+{
+	int num;
+	char letter;
 
-int main(void)
-{
-int n = 48;
-int l = 'a';
-while (n <= 57)
-{
-putchar(n);
-n += 1;
-}
-while (l <= 'f')
-{
-putchar(l);
-l++;
-}
-putchar('\n');
-return (0);
-}
+	for (num = 0; num < 10 num++)
+		putchar((num% 10) + '0');
 
+	for letter = 'a'; letter <='f' letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
