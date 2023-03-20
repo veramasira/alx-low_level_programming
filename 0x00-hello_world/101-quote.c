b5d7499d3884bad9f9fa67c8dae9432b0020d5d9
masiravera@gmail.com <masiravera@gmail.com>
2023-03-20 12:42:17 +0300

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,17 @@
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19",
+			followedby a new line, to standard error.
 
-	return (1);
+			
+	* Return: Always 1.
+	*/
+int main(void)
+{
+         write(2,
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+return (1)
 }
 
