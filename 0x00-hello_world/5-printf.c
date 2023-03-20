fab3814f4101a3bfaa8183cc0b9d0113c88640de
masiravera@gmail.com <masiravera@gmail.com>
2023-03-20 12:08:57 +0300

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+
 /**
- *  * Description: main - Prints out a string to stdout.
- *   * Return: 0 if success.
- *    */
+ * main - Entry point
+ *
+ * Return: 0 if success.
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
