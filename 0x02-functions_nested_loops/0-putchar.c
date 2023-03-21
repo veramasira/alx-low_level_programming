9a9e79bd375edfd2e6023dcc08004919180cd57e
masiravera@gmail.com <masiravera@gmail.com>
2023-03-21 15:30:13 +0300

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include "_putchar"
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(c)
+{
+	return (write(1, &c, 1));
+}
