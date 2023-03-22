bc48590ba1f62a33cae2f9ca50671e21d151a565
masiravera@gmail.com <masiravera@gmail.com>
2023-03-22 15:09:01 +0300

    104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - prints the first 90 fibonacci numbers starting by 1 and 2, separated by a comma followed by space
+ * Return: always 0
+ */
+int main(void)
+{
+	int count;
+	unsigned long fib1 = 0, fib2 = 1, sum ;
+	unsigned long fib1_half1, fib2_half2, 
+		      fib2_half1, fib3_half2;
+
+	for (count = 0; count < 90; count++)
+	{
+		sum = fib1 + fib2;
+		printf("%lu, ", sum);
+
+		fib1 = fib2;
+		fib2 = sum;
+	}
+	fib1_half1 = fib1 / 10000000000;
+	fib2_half1 = fib2 / 10000000000;
+	fib1_half2 = fib1 % 10000000000;
+	fib2_half2 = fib2 % 10000000000;
+
+	for(count = 93; count < 99; count++)
+	{
+		half1 = fib1_half1 + fib2_half1;
+		half2 = fib1_half2 + fib2_half2;
+
+		if(fib1_half2 + fib2_half2 < 9999999999)
+		{
+			half1 += 1;
+			half2 %= 10000000000;
+		}
+		printf("%lu%lu", half1, half2);
