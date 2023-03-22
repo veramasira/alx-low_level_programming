c59df4e3ba768e36aaabab5902b9f6e0ac9bb5e2
masiravera@gmail.com <masiravera@gmail.com>
2023-03-22 21:41:25 +0300

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,14 +1,18 @@
 #include "main.h"
+
 /**
- *  * print_last_digit - func is to check if c is a letter, lowercase or uppercase
- *   * @n: is the argument that will be used
- *    * Return: value of the last digit
- *     */
+ * print_last_digit - prints the last digit
+ * @n: the number
+ * Return: value of the last digit
+ */
 int print_last_digit(int n)
 {
-	int last_digit = n % 10;
-	if (last_digit < 0)
-		        last_digit *= -1;
-	_putchar (last_digit + '0');
-	return (last_digit);
-} 
+	int ld = n % 10;
+
+	if (ld < 0)
+		ld *= -1;
+
+	_putchar(ld + '0');
+
+	return (0);
+}
