a4a10c085b017ce6685c63cb69551de55a99f057
masiravera@gmail.com <masiravera@gmail.com>
2023-03-24 17:59:18 +0300

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,10 +1,8 @@
-#ifndef MAIN_H
-#define MAIN_H
-
+int _putchar(char a);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int _isalpha(int c);
 int _islower(int c);
+int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
@@ -12,6 +10,4 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-int _putchar(char c);
-
-#endif
+void print_times_table(int n);
