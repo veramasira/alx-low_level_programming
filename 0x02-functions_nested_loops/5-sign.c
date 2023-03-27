8428832f36f88c10fed58bf0930a020777442242
masiravera@gmail.com <masiravera@gmail.com>
2023-03-27 17:36:21 +0300

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,10 +1,14 @@
 #include "main.h"
+
 /**
- * print_sign - prints the sign of a number
- * @n: the sign of the number to be printed
- * Return: 1 if number is greater than zero
- * 0 if number is zero
+ * print_sign - prints sign of a number
+ *
+ * @c: character to compare
+ *
+ * Return: 1 if 1, 0 if 0, -1 if -1
+ *
  */
+
 int print_sign(int n)
 {
 	if (n > 0)
@@ -12,14 +16,14 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-	else if (n == 0)
-	{
-		_putchar('0');
-		return (0);
-	}
-	else
+	else if (n < 0)
 	{
 		_putchar('-');
 		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
 	}
 }
