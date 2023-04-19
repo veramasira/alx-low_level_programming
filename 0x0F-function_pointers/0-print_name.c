7d4e138d931bbbff6f889b892d3bac3aa6c6ff40
masiravera@gmail.com <masiravera@gmail.com>
2023-04-19 12:11:02 +0300

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * print_name - print the name
+ * @name: name to print
+ * @f: function to used
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
