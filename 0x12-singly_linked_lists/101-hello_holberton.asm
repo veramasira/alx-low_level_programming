87679625430c889e55884ee75e6be7bff1c2abb9
veramasira <masiravera@gmail.com>
2023-05-02 09:54:47 +0300

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,14 +1,9 @@
-section .data
-    hello: db "Hello, Holberton,", 10, 0  ; Define the string to be printed
-
-section .text
-    global main  ; Entry point for the program
-
+	global   main
+extern	printf
 main:
-    mov rax, 0  ; Clear the RAX register
-    lea rdi, [hello]  ; Load the address of the string into the RDI register
-    xor rsi, rsi  ; Clear the RSI register (second argument to printf)
-    mov eax, 0  ; Set EAX to 0 (indicating the first argument is a string)
-    call printf  ; Call the printf function to print the string
-    mov eax, 0  ; Set the exit code to 0 (indicating success)
-    ret  ; Return from the main function
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov   eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
