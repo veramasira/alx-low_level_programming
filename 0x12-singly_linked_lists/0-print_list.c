c5b7a2b2b7e67f8acb46961220ad3be65ae9a892
veramasira <masiravera@gmail.com>
2023-05-03 20:01:01 +0300

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 #include "lists.h"
-#include <stdlib.h>
-#include <stddef.h>
 
 /**
  * print_list - prints all the elements of a linked list
@@ -13,12 +11,12 @@ size_t print_list(const list_t *h)
 {
 	size_t s = 0;
 
-	while (h != NULL)
+	while (h)
 	{
-		if (h->str == NULL)
+		if (h->str)
 			printf("[0] (nil)\n");
 		else
-			printf("[%d] %s\n", h->len, h->str);
+			printf("[%u] %s\n", h->len, h->str);
 	h = h->next;
 	s++;
 	}
