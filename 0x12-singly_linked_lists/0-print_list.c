218c61ddea79cb748f257df74f7e82689d7e3037
veramasira <masiravera@gmail.com>
2023-05-03 20:12:22 +0300

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,12 +13,12 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-		if (h->str)
+		if (!h->str)
 			printf("[0] (nil)\n");
 		else
 			printf("[%u] %s\n", h->len, h->str);
-	h = h->next;
-	s++;
+		h = h->next;
+		s++;
 	}
 
 	return (s);
