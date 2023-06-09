dd8172267c09ca5e7e84982633a827ee043593ed
veramasira <masiravera@gmail.com>
2023-06-09 12:20:01 +0300

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,7 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
- * main - prints its name followed by a new line
+ * main - prints its name, followed by a new line
  * @argc: argument count
  * @argv: arguments
  *
